717adb3fef7fe9bbee247a4b6cceea294b542dcc
AtanasKisyov <atanaskisyov9@gmail.com>
2022-05-20 19:51:04 +0300

    add celery settings

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -100,3 +100,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
